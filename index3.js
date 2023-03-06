8ee500e660cef07f8444bd05973b9668c4aedf8a
Everton Figueiredo <j.everton.figueiredo@gmail.com>
2023-03-06 20:20:59 -0300

    Arrays

---
+++ index3.js
@@ -0,0 +1,87 @@
+// let produto1 = "Caneta"
+// let preco1 = 2.5
+// let qtd1 = 2
+
+// console.log(produto1,preco1,qtd1);
+
+
+// let produto2  = "Lapis"
+// let preco2 = 1.0
+// let qtd2 = 10
+
+// console.log(produto2,preco2,qtd2);
+
+// let produtos = [
+//     "Caneta",
+//     "Lapis",
+//     "Borracha"
+// ]
+
+// let produto1 = [
+//     "Caneta",
+//     2.5,
+//     2
+// ]
+
+// let produto2 = [
+//     "Lapis",
+//     1.5,
+//     10
+// ]
+
+// ["Caneta", 2.5, 2],
+// ["Lapis", 1.5, 10],
+// ["Borracha", 0.75, 12]
+
+
+
+// console.log(produtos);
+
+// produtos.push(["Caneta", 2.5, 2])
+
+// console.log(produtos);
+
+// produtos.push(["Lapis", 1.5, 10])
+
+// console.log(produtos);
+
+// let produtos = []
+
+// for (let index = 0; index < 5; index++) {
+    
+//     let nome = prompt("Nome do produto")
+//     let preco = prompt("Preço do produto")
+//     let qtd = prompt("Qtd do produto")
+
+//     let produto = [
+//         nome,
+//         preco,
+//         qtd
+//     ]
+
+//     produtos.push(nome)
+    
+// }
+
+// console.log(produtos);
+
+let usuarios = []
+
+
+// Adicionar um elemento no final do array
+usuarios.push("Danielle")
+
+console.log(usuarios);
+
+// Remove o ultimo elemento
+// usuarios.pop()
+
+// Remove o primeiro elemento
+// usuarios.shift()
+
+// Adiciona um elemento no primeiro index do array
+usuarios.unshift("Sanderson")
+
+console.log(usuarios)
+
+
